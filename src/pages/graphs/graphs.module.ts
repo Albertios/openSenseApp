fb5d26adbddc1a499febf576edbeba2bd7f0821e
sholtkamp <S.Kokon93@gmail.com>
2019-01-28 14:08:10 +0100

    cleanup: info

--- src/pages/graphs/graphs.module.ts
+++ src/pages/graphs/graphs.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { GraphsPage } from './graphs';
 
-
 @NgModule({
   declarations: [
     GraphsPage,
--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the InfoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-info',
@@ -23,5 +16,4 @@ export class InfoPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoPage');
   }
-
 }
